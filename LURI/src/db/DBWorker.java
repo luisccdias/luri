c9b1964d3d46ee870220457dcafc21dda005f3d7
luisccdias <94022828+luisccdias@users.noreply.github.com>
2022-04-12 14:16:53 +0100

    Add files via upload

---
+++ LURI/src/db/DBWorker.java
@@ -0,0 +1,56 @@
+
+package db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+
+/**
+ *
+ * @author Rui Boticas
+ * @version 1.0.2
+ */
+public class DBWorker {
+
+    private final String servidor;
+    private final String user;
+    private final String password;
+    private final String baseDados;
+    private final String url;
+
+    private final Connection conn;
+
+    public DBWorker(String servidor, String user, String password, String baseDados) throws SQLException {
+        this.servidor = servidor;
+        this.user = user;
+        this.password = password;
+        this.baseDados = baseDados;
+        this.url = "jdbc:mysql://" + this.servidor + ":3306/" + this.baseDados + "?serverTimezone=UTC";
+        conn = getConnection();
+    }
+
+    private Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(url, user, password);
+    }
+
+    public int executeUpdate(String sql) throws SQLException {
+        final Statement st = conn.createStatement();
+        int resultado = st.executeUpdate(sql);
+        return resultado;
+    }
+
+    public ResultSet executeQuery(String sql) throws SQLException {
+        final Statement st = conn.createStatement();
+        return st.executeQuery(sql);
+    }
+    
+    public void closeConnection() throws SQLException {
+
+        if (conn != null) {
+            conn.close();
+        }
+    }
+}
